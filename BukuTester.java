68ce1ac6f165ab69e069b3518ee46e221c214985
AlfiAprilia <alfi_fatimah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 21:52:16 +0700

    Add files via upload

---
+++ BukuTester.java
@@ -0,0 +1,58 @@
+public class BukuTester {
+    public static void main(String[] args) {
+        //object
+       Buku Bulan  = new Buku();
+       Buku Langit = new Buku();
+       Buku Pluto = new Buku();
+       Buku Saku = new Buku();
+       Buku Laut = new Buku();
+
+       //mengubah nilai variabel
+       Bulan.id = 123;
+       Bulan.judul = "Bulan";
+       Bulan.Pengarang = "Jia";
+       Bulan.Tahunterbit = 2002;
+       
+       Langit.id = 345;
+       Langit.judul = "Langit";
+       Langit.Pengarang = "Lian";
+       Langit.Tahunterbit = 2012;
+
+       Pluto.id = 567;
+       Pluto.judul = "Pluto";
+       Pluto.Pengarang = "Javi";
+       Pluto.Tahunterbit = 2009;
+
+       Saku.id = 789;
+       Saku.judul = "Saku";
+       Saku.Pengarang = "Den";
+       Saku.Tahunterbit = 2016;
+
+       Laut.id = 986;
+       Laut.judul = "Laut";
+       Laut.Pengarang = "Vier";
+       Laut.Tahunterbit = 2006;
+
+       System.out.println(Bulan.id);
+       System.out.println(Bulan.judul);
+       System.out.println(Bulan.Pengarang);
+       System.out.println(Bulan.Tahunterbit);
+       System.out.println(Langit.id);
+       System.out.println(Langit.judul);
+       System.out.println(Langit.Pengarang);
+       System.out.println(Langit.Tahunterbit);
+       System.out.println(Pluto.id);
+       System.out.println(Pluto.judul);
+       System.out.println(Pluto.Pengarang);
+       System.out.println(Pluto.Tahunterbit);
+       System.out.println(Saku.id);
+       System.out.println(Saku.judul);
+       System.out.println(Saku.Pengarang);
+       System.out.println(Saku.Tahunterbit);
+       System.out.println(Laut.id);
+       System.out.println(Laut.judul);
+       System.out.println(Laut.Pengarang);
+       System.out.println(Laut.Tahunterbit);
+              
+    }
+}
